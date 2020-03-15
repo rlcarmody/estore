77e4ae6724870fd2aebb81fc45ad2dd474439cef
RLC <rlcarmody@gmail.com>
2020-03-14 19:30:18 -0700

    Create styled button component

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { lighten } from 'polished';
+
+export const Button = styled.button`
+  background-color: ${({ primary }) => primary ? '#296800' : '#FFF'};
+  color: ${({ primary }) => primary ? '#FFF' : '#296800'};
+  padding: 4px;
+  min-width: 7rem;
+  height: 3rem;
+  border: 2px solid #296800;
+  box-shadow: 2px 2px 3px -2px rgba(0,0,0,.5);
+  border-radius: .25rem;
+  font-family: 'Martel Sans', sans-serif;
+  &:hover {
+    background-color: ${({ primary }) => primary ? lighten(.1, '#296800') : '#FFF'};
+    border-color: ${lighten(.1, '#296800')};
+    color: ${({ primary }) => primary ? '#FFF' : lighten(.1, '#296800')};
+    cursor: pointer;
+  }
+`;
+
+export default Button
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '../components'
+
+export default { title: 'Button', component: Button }
+
+export const Primary = () => <Button primary>Primary Button</Button>
+
+export const Secondary = () => <Button>Secondary Button</Button>
